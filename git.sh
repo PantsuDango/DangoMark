4745b12cf5e7d088bafc455e1094f1e2c99bc5bd
Bard <lsy1219941031>
2021-06-06 14:39:36 +0800

    项目初始化

--- git.sh
+++ git.sh
@@ -6,17 +6,17 @@ path=$PWD
 cd $path/service
 num=$(netstat -nlpt | grep ":::3000" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
-    kill -9 $(ps -ef | grep "job-hunting" | grep -v "grep" | awk '{print $2}')
+    kill -9 $(ps -ef | grep "DangoMark" | grep -v "grep" | awk '{print $2}')
     sleep 3
 fi
-nohup go run job-hunting.go &
+nohup go run DangoMark.go &
 sleep 3
 
 num=$(netstat -nlpt | grep ":::3000" | grep -v "grep" | wc -l)
 if [ $num -eq 1 ]; then
-    echo "run job-hunting success"
+    echo "run DangoMark success"
 else
     cd $path/app
     tail -n 10 nohup.out
-    echo "run job-hunting fail"
+    echo "run DangoMark fail"
 fi
